43092f2f7ef5d8e46c07409a71cc709599ed5ded
NeeleK <45965251+NeeleK@users.noreply.github.com>
2021-07-29 17:31:59 +0200

    Mit main Methode

--- temperatur_raum_abfragen.py
+++ temperatur_raum_abfragen.py
@@ -23,7 +23,14 @@ messwerte_room_sensors = {
 
     }
 
-messwerte_room_pi = {
+messwerte_indoor_pi = {
+    "TVOC" : 0,
+    "eCO2" : 0,
+    "humidity" : 0,
+    "temperature" : 0
+    }
+
+messwerte_outdoor_pi = {
     "TVOC" : 0,
     "eCO2" : 0,
     "humidity" : 0,
@@ -31,9 +38,45 @@ messwerte_room_pi = {
     }
 
 #Temperatur, die im Raum angestrebt wird
-room_temperature_want = 30.0
+room_temperature_want = 20.0
+CO2_want = 800.0
 
-#check if temperature too high or low
+room_has_pi = True
+
+################################################################
+#Notifications
+################################################################
+def send_open_window():
+    print("Bitte öffnen Sie das Fenster.")
+
+
+################################################################
+#CO2 check
+################################################################
+
+#Ziel sollte sein, einen CO2-Wert von 1.000 ppm nicht zu überschreiten. 1.400 ppm ist die obere Grenze für akzeptable Raumluft.
+
+def run_CO2():
+    if messwerte_indoor_pi.get("eCO2") > CO2_want:
+        send_open_window()
+
+
+################################################################
+#CO2 check
+################################################################
+
+#Ziel sollte sein, einen CO2-Wert von 1.000 ppm nicht zu überschreiten. 1.400 ppm ist die obere Grenze für akzeptable Raumluft.
+
+def run_CO2():
+    if messwerte_indoor_pi.get("eCO2") > CO2_want:
+        send_open_window()
+
+
+
+################################################################
+#Temperature-Check
+#Temperature to high or to low?
+################################################################
 def check_temperature(temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open):
     #check if room temperature too high
     if temperature > (room_temperature_want - temperature_difference):
@@ -59,8 +102,13 @@ def check_temperature(temperature, room_temperature_want, temperature_difference
            #send_run_heating()
             print("run heating")
 
-def send_open_window():
-    print("Bitte öffnen Sie das Fenster.")
+def run_temperature_check():
+    temperature = messwerte_room_sensors.get("temperature")
+    outdoor_temperature = messwerte_Building.get("outdoorTemperature")
+    window_open = messwerte_room_sensors.get("windowsOpen")
+
+    check_temperature(temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open)
+
 
 
 
@@ -74,20 +122,27 @@ def set_messwerte_room_sensors(dictionary):
 def set_messwerte_room_pi(dictionary):
     messwerte_room_pi = dictionary
 
+def set_messwerte_outdoor_pi(dictionary):
+    messwerte_room_pi = dictionary
+
 def set_room_temperature(value):
     raum_temperatur = value
 
+def set_room_has_pi(bool):
+    room_has_pi = bool
 
-def run_temperature_check():
-    temperature = messwerte_room_sensors.get("temperature")
-    outdoor_temperature = messwerte_Building.get("outdoorTemperature")
-    window_open = messwerte_room_sensors.get("windowsOpen")
 
-    check_temperature(temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open)
+################################################################
+#Main Function
+################################################################
 
 
 def main():
     run_temperature_check()
+
+    if room_has_pi:
+        run_CO2()
+
 
 if __name__ == "__main__":
     # execute only if run as a script
