5eb02ab90b9c17eeaa579389e19869400d251711
NeeleK <45965251+NeeleK@users.noreply.github.com>
2021-07-29 18:46:15 +0200

    Vollständig

--- temperatur_raum_abfragen.py
+++ temperatur_raum_abfragen.py
@@ -1,149 +1,147 @@
-#Constants
-# Ab welchem Unterschied wird eine Notification rausgegeben?
-temperature_difference = 2
+class RoomCheck:
 
-messwerte_Building = {
-    "totalPowerConsumption": 15.49,
-    "powerConsumptionDataCenter": 14.78,
-    "solarPowerOutput": 16.13,
-    "outdoorTemperature": 25,
-    "waterConsumption": 40.13,
-    "totalEmployeesIn": 24
-    }
+    def __init__(self, temperature_difference, room_temperature_want, CO2_want, CO2_danger, humidity_want, TVOC_good, TVOC_danger):
+        # Ab welchem Unterschied wird eine Notification rausgegeben?
+        self.temperature_difference = temperature_difference
 
-# ein Beispielraum
-messwerte_room_sensors = {
-        "lightOn": False,
-        "windowsOpen": False,
-        "rollerBlindsClosed": False,
-        "airConditioningRunning": False,
-        "heaterRunning": True,
-        "temperature": 20.369021225136912,
-        "powerConsumption": 0.02785408418197808
+        self.room_temperature_want = room_temperature_want
+        self.CO2_want = CO2_want
+        self.CO2_danger = CO2_danger
+        self.humidity_want = humidity_want
+        self.TVOC_good = TVOC_good
+        self.TVOC_danger = TVOC_danger
 
-    }
+    ################################################################
+    #Notifications
+    ################################################################
+    def send_open_window():
+        print("Bitte öffnen Sie das Fenster.")
 
-messwerte_indoor_pi = {
-    "TVOC" : 0,
-    "eCO2" : 0,
-    "humidity" : 0,
-    "temperature" : 0
-    }
+    def send_TVOC_outdoor_alarm():
+        print("Bitte öffnen Sie keine Fenster.")
 
-messwerte_outdoor_pi = {
-    "TVOC" : 0,
-    "eCO2" : 0,
-    "humidity" : 0,
-    "temperature" : 0
-    }
+    def send_TVOC_indoor_alarm():
+        print("Sehr hoher VOC-Wert im Raum, bitte verlassen Sie umgehend das Gebäude.")
 
-#Temperatur, die im Raum angestrebt wird
-room_temperature_want = 20.0
-CO2_want = 800.0
+    def send_CO2_indoor_alarm():
+        print("Sehr hoher CO2-Wert im Raum, bitte verlassen Sie umgehend das Gebäude.")
 
-room_has_pi = True
+    def send_temperature_indoor_alarm():
+        print("Sehr hohe Temperatur im Raum, bitte verlassen Sie umgehend das Gebäude.")
 
-################################################################
-#Notifications
-################################################################
-def send_open_window():
-    print("Bitte öffnen Sie das Fenster.")
+    ################################################################
+    #TVOC check
+    ################################################################
+
+    #Das Umweltbundesamt gibt Empfehlungen für das Vorkommen von Flüchtigen Organischen Verbindungen (VOCs) von hygienisch unbedenklich (unter 1 mg/m³ – unter 150 ppb) über hygienisch auffällig (zwischen 1 bis 3 mg/m³ – 150 bis 1300 ppb) bis hin zu hygienisch inakzeptabel (über 10 mg/m³ – über 1500 bis 4000 ppb)
+
+    def check_outside_TVOC(self,messwerte_outdoor_pi):
+        if messwerte_outdoor_pi.get("TVOC") > self.TVOC_danger:
+            self.send_TVOC_outdoor_alarm()
 
 
-################################################################
-#CO2 check
-################################################################
-
-#Ziel sollte sein, einen CO2-Wert von 1.000 ppm nicht zu überschreiten. 1.400 ppm ist die obere Grenze für akzeptable Raumluft.
-
-def run_CO2():
-    if messwerte_indoor_pi.get("eCO2") > CO2_want:
-        send_open_window()
-
-
-################################################################
-#CO2 check
-################################################################
-
-#Ziel sollte sein, einen CO2-Wert von 1.000 ppm nicht zu überschreiten. 1.400 ppm ist die obere Grenze für akzeptable Raumluft.
-
-def run_CO2():
-    if messwerte_indoor_pi.get("eCO2") > CO2_want:
-        send_open_window()
-
-
-
-################################################################
-#Temperature-Check
-#Temperature to high or to low?
-################################################################
-def check_temperature(temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open):
-    #check if room temperature too high
-    if temperature > (room_temperature_want - temperature_difference):
+    def check_inside_TVOC(self, messwerte_indoor_pi, messwerte_outdoor_pi):
+        if messwerte_indoor_pi.get("TVOC") > self.TVOC_danger:
+            self.send_TVOC_indoor_alarm()
         
-        #if it is colder outside
-        if outdoor_temperature < temperature:
+        elif messwerte_indoor_pi.get("TVOC") > self.TVOC_good:
+            if messwerte_outdoor_pi.get("TVOC") < messwerte_indoor_pi.get("TVOC"):
+                self.send_open_window()
+
+
+    ################################################################
+    #CO2 check
+    ################################################################
+
+    #Ziel sollte sein, einen CO2-Wert von 1.000 ppm nicht zu überschreiten. 1.400 ppm ist die obere Grenze für akzeptable Raumluft.
+
+    def run_CO2(self, messwerte_indoor_pi):
+
+        if messwerte_indoor_pi.get("eCO2") > self.CO2_danger:
+            self.send_CO2_indoor_alarm()
+
+        if messwerte_indoor_pi.get("eCO2") > self.CO2_want:
+            self.send_open_window()
+
+
+    ################################################################
+    #Humidity check
+    ################################################################
+
+    #Die optimale Luftfeuchtigkeit ist keine feste Größe, sondern eine prozentuale Spanne. Als optimal gilt eine relative Luftfeuchtigkeit von etwa 50%, mit einer Abweichung von +/- 10%.
+
+    def run_humidity(self, messwerte_indoor_pi,messwerte_outdoor_pi):
+        humidity_inside = messwerte_indoor_pi.get("humidity")
+        humidity_outdoor = messwerte_outdoor_pi.get("humidity")
+        
+        #if humidity is too high
+        if humidity_inside > humidity_inside + 10.0:
+
+            if humidity_outdoor < humidity_inside:
+                self.send_open_window()
+        
+        #if humidity is too low
+        elif humidity_inside < humidity_inside - 10.0:
+            if humidity_outdoor > humidity_inside:
+                self.send_open_window()
+
+
+
+    ################################################################
+    #Temperature-Check
+    #Temperature to high or to low?
+    ################################################################
+    def check_temperature(self, temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open):
+        #check if room temperature too high
+        if temperature > (room_temperature_want - temperature_difference):
             
-            if window_open == False:
-                send_open_window()
+            #if it is colder outside
+            if outdoor_temperature < temperature:
+                
+                if window_open == False:
+                    self.send_open_window()
+            
+            else:
+                #send_run_air_conditioning()
+                print("run air cond")
         
-        else:
-            #send_run_air_conditioning()
-            print("run air cond")
-    
-    #check if room temperature too low
-    elif temperature < (room_temperature_want + temperature_difference):
-        #if it is warmer outside
-        if outdoor_temperature > temperature:
-            if window_open == False:
-                send_open_window()
+        #check if room temperature too low
+        elif temperature < (room_temperature_want + temperature_difference):
+            #if it is warmer outside
+            if outdoor_temperature > temperature:
+                if window_open == False:
+                    self.send_open_window()
+            
+            else:
+            #send_run_heating()
+                print("run heating")
+
+    def run_temperature_check(self, messwerte_room_sensors, messwerte_building):
+        temperature = messwerte_room_sensors.get("temperature")
+        outdoor_temperature = messwerte_building.get("outdoorTemperature")
+        window_open = messwerte_room_sensors.get("windowsOpen")
+
+        self.check_temperature(temperature, self.room_temperature_want, self.temperature_difference, outdoor_temperature, window_open)
+
+
+
+
+    ################################################################
+    #Main Function
+    ################################################################
+
+
+    def main(self, messwerte_building,messwerte_room_sensors, messwerte_indoor_pi, messwerte_outdoor_pi):
         
-        else:
-           #send_run_heating()
-            print("run heating")
+        self.run_temperature_check(messwerte_room_sensors, messwerte_building)
 
-def run_temperature_check():
-    temperature = messwerte_room_sensors.get("temperature")
-    outdoor_temperature = messwerte_Building.get("outdoorTemperature")
-    window_open = messwerte_room_sensors.get("windowsOpen")
+        if messwerte_indoor_pi:
+            self.run_CO2(messwerte_indoor_pi)
 
-    check_temperature(temperature, room_temperature_want, temperature_difference, outdoor_temperature, window_open)
+            if messwerte_outdoor_pi:
+                self.run_humidity(messwerte_indoor_pi,messwerte_outdoor_pi)
 
 
-
-
-#Set functions
-def set_messwerte_building(dictionary):
-    messwerte_building = dictionary
-
-def set_messwerte_room_sensors(dictionary):
-    messwerte_room_sensors = dictionary
-
-def set_messwerte_room_pi(dictionary):
-    messwerte_room_pi = dictionary
-
-def set_messwerte_outdoor_pi(dictionary):
-    messwerte_room_pi = dictionary
-
-def set_room_temperature(value):
-    raum_temperatur = value
-
-def set_room_has_pi(bool):
-    room_has_pi = bool
-
-
-################################################################
-#Main Function
-################################################################
-
-
-def main():
-    run_temperature_check()
-
-    if room_has_pi:
-        run_CO2()
-
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+    if __name__ == "__main__":
+        # execute only if run as a script
+        main()
